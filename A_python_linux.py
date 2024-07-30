ef4925f3d1aa1e228e5cdc44a2e14a36c6b5b98c
georgemaxwell47 <akadkhoda81@gmail.com>
2024-07-30 15:55:17 +0330

    NULL_LINUX

--- A_python_linux.py
+++ A_python_linux.py
@@ -1,27 +1,11 @@
 import serial.tools.list_ports , serial , time, keyboard
+import numpy as np
 # in linux version first you must root and active env !
 
 # for root su and sudo -s 
 # for active env source env/bin/activate 
-port = serial.tools.list_ports.comports()
-
-for i in range(len(port)):
-    print((str(port[i])))
-
-print("which one?")
-if(str(port[0]).startswith("COM")):
-    selectport = int(input("COM"))
-    selectport = "COM"+str(selectport)
-elif(str(port[0]).startswith("/dev/ttyUSB")):
-    selectport = int(input("/dev/ttyUSB"))
-    selectport = "/dev/ttyUSB"+ str(selectport)
-else:
-    print("WTF")
-
-buadrate = int(input("baudrate "))
-
-check = int(input("OK ,  "+ str(selectport) +" WITH BAUD RATE "+str(buadrate) +" HAS BEEN SELECTED . PLZ PRESS 0 TO CANCEL OR 1 TO PROCESS "))
-while(check == 0):
+def detect():
+    mdict = {} 
     port = serial.tools.list_ports.comports()
 
     for i in range(len(port)):
@@ -29,40 +13,64 @@ while(check == 0):
 
     print("which one?")
     if(str(port[0]).startswith("COM")):
-        selectport = int(input("COM"))
-        selectport = "COM"+str(selectport)
+        while(True):
+            vin = input("COM")
+            if(vin.isdigit()):
+                mdict['selport'] = "COM"+str(vin)
+                break
     elif(str(port[0]).startswith("/dev/ttyUSB")):
         selectport = int(input("/dev/ttyUSB"))
-        selectport = "/dev/ttyUSB"+ str(selectport)
+        mdict['selport'] = "/dev/ttyUSB"+ str(selectport)
     else:
         print("WTF")
 
-    buadrate = int(input("baudrate "))
+    mdict['baudrate'] = int(input("baudrate "))
 
-    check = int(input("OK ,"+ str(selectport) +" WITH BAUD RATE "+str(buadrate) +" HAS BEEN SELECTED . PLZ PRESS 0 TO CANCEL OR 1 TO PROCESS "))
+    mdict['check'] = int(input("OK ,  "+ str(mdict['selport']) +" WITH BAUD RATE "+str(mdict['baudrate']) +" HAS BEEN SELECTED . PLZ PRESS 0 TO CANCEL OR 1 TO PROCESS "))
+    return mdict
 
-uart = serial.Serial(port=selectport,baudrate=buadrate,timeout=2)
+mode = 0
+
+Hdict = detect()
+while(Hdict['check'] == 0):
+    Hdict = detect()
+
+uart = serial.Serial(port=Hdict['selport'],baudrate=Hdict['baudrate'],timeout=2)
 #uart.open()
+arr = []
+arr2 = []
+#print(arr)
 while(True):
-    print(uart.read())
+    if(mode == 0):
+        print(uart.read())
+    elif(mode == 1):
+        arr.append(uart.read())
+        #print(arr)
+        for i in range (len(arr)):
+            if(arr[i] != b'\x00'):
+                arr2.append(int(arr[i]))
+            else:
+                print(','.join(map(str,arr2)))
+                arr2.clear()
+
     if(keyboard.is_pressed('q')):
+        uart.close()
         exit()
-    
 
+    if(keyboard.is_pressed('e')):
+        uart.close()
+        Hdict = detect()
+        uart = serial.Serial(port=Hdict['selport'],baudrate=Hdict['baudrate'],timeout=2)
 
-'''
-if(uart.is_open):
-    while(True):
-        uart.in_waiting()
+    if(keyboard.is_pressed('n')):
+        print("YOU WANT TO REPLACE NULL WITH NEW LINE .PLZ PRESS 0 TO CANCEL OR 1 TO PROCESS ,PRESS 3 == DEFULT ")
+        while(True):
+            vin = input()
+            if(vin.isdigit()):
+                #print(type(vin))
+                if(vin == str(3)):
+                    mode = 0
+                if(vin == str(1)):
+                    mode = 1
 
-        if (size != 0):
-            data = uart.read(size)
-            print(data)
-
-        else:
-            print("NO DATA")
-            time.sleep(1)
-
-else:
-    print("UART NOT OPEN")
-'''
+                break   
